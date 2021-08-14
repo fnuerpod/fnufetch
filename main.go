ba55b2d795a48453ae2b2470acd185a22790fdca
fnuer <88922673+fnuerpod@users.noreply.github.com>
2021-08-14 08:53:24 +0100

    improve distro forcing/detection

--- main.go
+++ main.go
@@ -15,15 +15,18 @@ import (
 func main() {
 	os_struct := os_release.NewOSR()
 
-	forceDistroFlag := flag.String("force_distro", os_struct.PrettyName, "Forces distribution name - overrides /etc/os-release PrettyName variable.")
+	forceDistroFlag := flag.String("force_distro", os_struct.Name, "Forces distribution name - overrides /etc/os-release PrettyName variable.")
 
 	flag.Parse()
 
+	var sys_logo []string
+
 	if *forceDistroFlag != "" {
 		// force flag set.
-		os_struct.PrettyName = *forceDistroFlag
+		sys_logo = logos.GetLogo(*forceDistroFlag)
+	} else {
+		sys_logo = logos.GetLogo(os_struct.Name)
 	}
-	sys_logo := logos.GetLogo(os_struct.PrettyName)
 
 	// make strings for injection
 	inject_strings := []string{
